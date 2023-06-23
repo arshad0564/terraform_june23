80fc96b049d57b6b8347b357e40888b4e9434117
arshad0564 <syedarshad0564@gmail.com>
2023-06-23 16:13:58 +0530

    .

---
+++ inst.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "jenkins_terraform_aws" {
+  ami           = "ami-0331ebbf81138e4de"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "jenkins_terraform"
+  }
+}
